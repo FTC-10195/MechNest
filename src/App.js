84e415b1d3bf5659ff6b3e64e8c5c1d4ca37882c
Z-M-Man <112212530+Z-M-Man@users.noreply.github.com>
2024-12-13 19:33:31 -0500

    Super basic Card system. Definitely not how it will end up looking but just a proof-of-concept.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from './Images/nightowlslogo.png';
+import { useState } from 'react';
 import './App.css';
+import Card from './Card';
+import logo from './Images/nightowlslogo.png';
 import './index.css';
 //Use the 'App.css to give color, shape, size, style, etc
 function App() {
+    const [selectedCard, setSelectedCard] = useState(null);
+    const cards = [
+        { id: 1, title: 'Test 1', description: 'Lorem ipsum'},
+        { id: 2, title: 'Test 2', description: 'Qui dolorem ipsum'}
+    ];
+
+    const handleCardClick = (card) => {setSelectedCard(card);};
+
     return (
     <div className="App">
       <title>{ "CAD Website" }</title>
@@ -25,6 +35,24 @@ function App() {
             <img src={logo} alt="logo" className = "App-logo"/>
         </a>
       </header>
+
+      <div className="card-container">
+      {cards.map((card) => ( 
+        <Card
+            key={card.id}
+            title={card.title}
+            description={card.description}
+            onClick={() => handleCardClick(card)}
+        />
+      ))}
+      </div>
+
+      {selectedCard && (<div className="card-details">
+          <h2>{selectedCard.title}</h2>
+          <p>{selectedCard.description}</p>
+          <button onClick={() => setSelectedCard(null)}>Close</button>
+          </div>
+      )}
      
       CENTER
       <p>
